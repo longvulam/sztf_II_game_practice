c8885b3a019a9aa8d9c229dbd5b1aa308990e683
DESKTOP-UM6ON1L\g <masquadel@gmail.com>
2019-02-19 16:40:22 +0100

    Added MozgasHalalMiattNemSikerultKivetel exception.

---
+++ sztf_II_FF_practice/Szabalyok/MozgasHalalMiattNemSikerultKivetel.cs
@@ -0,0 +1,14 @@
+using System;
+using OE.Prog2.Jatek.Jatekter;
+
+namespace OE.Prog2.Jatek.Szabalyok
+{
+    public class MozgasHalalMiattNemSikerultKivetel : MozgasNemSikerultKivetel
+    {
+        public MozgasHalalMiattNemSikerultKivetel(int x, int y, JatekElem jatekElem) 
+            : base(x, y, jatekElem)
+        {
+
+        }
+    }
+}
