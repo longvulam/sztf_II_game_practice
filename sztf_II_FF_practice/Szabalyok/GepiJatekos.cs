3e289bfd1486eb61be1e10811cae472deb36522e
DESKTOP-UM6ON1L\g <masquadel@gmail.com>
2019-02-20 13:31:24 +0100

    Handling Cpu player not enough space exceptions.

--- sztf_II_FF_practice/Szabalyok/GepiJatekos.cs
+++ sztf_II_FF_practice/Szabalyok/GepiJatekos.cs
@@ -18,6 +18,23 @@ namespace OE.Prog2.Jatek.Szabalyok
 
         public void Mozgas()
         {
+            bool eThrown;
+            do
+            {
+                try
+                {
+                    eThrown = false;
+                    Move();
+                }
+                catch (MozgasHelyHianyMiattNemSikerult e)
+                {
+                    eThrown = true;
+                }
+            } while (eThrown);
+        }
+
+        private void Move()
+        {
             int r = Rand.Next(4);
             if (r == 0) Megy(-1, 0);
             if (r == 1) Megy(1, 0);
